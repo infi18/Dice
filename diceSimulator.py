cd43066039d2473a400d68b788d705a67700bf89
Siddhi Naik <siddhi.naik18@gmail.com>
2021-03-08 02:07:51 -0600

    Add files via upload
    Dice Simulator is a python based application that generates a random number from 1 to 6

---
+++ diceSimulator.py
@@ -0,0 +1,55 @@
+import random
+
+
+def diceSimulator():
+    print("Welcome to Dice Simulator !!")
+    while x == "y":
+
+        number = random.randint(1, 6)
+
+        if number == 1:
+            print("----------")
+            print("|        |")
+            print("|    O   |")
+            print("|        |")
+            print("----------")
+
+        if number == 2:
+            print("----------")
+            print("|        |")
+            print("| O    O |")
+            print("|        |")
+            print("----------")
+
+        if number == 3:
+            print("----------")
+            print("|    O   |")
+            print("|    O   |")
+            print("|    O   |")
+            print("----------")
+
+        if number == 4:
+            print("----------")
+            print("| O    O |")
+            print("|        |")
+            print("| O    O |")
+            print("----------")
+
+        if number == 5:
+            print("----------")
+            print("| O    O |")
+            print("|    O   |")
+            print("| O    O |")
+            print("----------")
+
+        if number == 6:
+            print("----------")
+            print("| O    O |")
+            print("| O    O |")
+            print("| O    O |")
+            print("----------")
+
+        x = input("Do you wish to continue ? Press y to roll again : ")
+
+diceSimulator()
+
